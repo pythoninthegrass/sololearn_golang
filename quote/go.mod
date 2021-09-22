2f91b981954a9a2f8d89107262cf7fff090bf191
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2021-09-21 23:41:05 -0500

    hello, world!

---
+++ quote/go.mod
@@ -0,0 +1,5 @@
+module example/quote
+
+go 1.16
+
+require rsc.io/quote v1.5.2
---
+++ quote/quote.go
@@ -0,0 +1,13 @@
+// SOURCE: https://golang.org/doc/tutorial/getting-started
+
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(quote.Go())
+}
