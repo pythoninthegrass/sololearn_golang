f54d44d2289f7bcdb20c5066b02c8eace80c6587
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2022-07-17 19:13:39 -0500

    38 Code Project: Match Results [case statement ver]
    If/else cond -> case statement, broke up comment into 2 lines

--- array_range_map/code_project/match_results.go
+++ array_range_map/code_project/match_results.go
@@ -10,22 +10,23 @@ func main() {
 	// l: 0
     results := []string{"w", "l", "w", "d", "w", "l", "l", "l", "d", "d", "w", "l", "w", "d"}
 
-	// take the last match result as input and append it to the results array. After that, calculate and output the total points the team gained from the results.
+	// Take the last match result as input and append it to the results array.
+	// After that, calculate and output the total points the team gained from the results.
 	var lastMatchResult string
 	// fmt.Printf("You winning, son? (w/d/l)\n")
 	fmt.Scanln(&lastMatchResult)
 	results = append(results, lastMatchResult)
 
-	// TODO: refactor as case statement
 	var totalPoints int
 	for _, result := range results {
 		// lowercase the result
 		result = strings.ToLower(result)
-		if result == "w" {
+		switch result {
+		case "w":
 			totalPoints += 3
-		} else if result == "d" {
+		case "d":
 			totalPoints += 1
-		} else {
+		default:
 			totalPoints += 0
 		}
 	}
